bb3b882d542e1476930ad1f4a3c1be8dc63c0ee1
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-14 18:09:40 +0700

    New commit

--- src/fileStreams/FileTyper.java
+++ src/fileStreams/FileTyper.java
@@ -1,7 +1,24 @@
 package fileStreams;
 
+import java.io.*;
+
 /**
  * Created by anonymous.vn1985@gmail.com
  */
 public class FileTyper {
+    public static void typeFile(String filename) throws IOException {
+        FileInputStream input = new FileInputStream(filename);
+        try {
+            StreamCopier.copy(input, System.out);
+        } finally {
+            input.close();
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
+            System.out.println("Usage: file typer");
+            return;
+        }
+        typeFile(args[0]);
+    }
 }
---
+++ src/fileStreams/StreamCopier.java
@@ -0,0 +1,26 @@
+package fileStreams;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+public class StreamCopier {
+    public static void copy(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[1024];
+        while (true) {
+            int bytesRead = input.read(buffer);
+            if (bytesRead == -1) break;
+            output.write(buffer,0,bytesRead);
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            copy(System.in, System.out);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}

05031f7e4ab208dfb79d2140673fded4697c1938
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-16 16:43:39 +0700

    New Commit

---
+++ src/networkStreams/URLs/URLTyper.java
@@ -0,0 +1,14 @@
+package networkStreams.URLs;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * Created by anonymous.vn1985@gmail.com
+ */
+
+public class URLTyper {
+    public static void main(String[] args) {
+
+    }
+}

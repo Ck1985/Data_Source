6342203fa0a79c90cd424ad70e931a19c828f18f
anonymous.vn1985 <anonymous.vn1985@gmail.com>
2017-05-19 18:26:07 +0700

    New commit

--- src/networkStreams/sockets/SocketTyper.java
+++ src/networkStreams/sockets/SocketTyper.java
@@ -9,6 +9,54 @@ import java.io.*;
 
 public class SocketTyper {
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            System.out.println("Usage: java SocketTyper url");
+            return;
+        }
+        try {
+            URL url = new URL(args[0]);
+            if (!url.getProtocol().equalsIgnoreCase("http")) {
+                System.out.println("Sorry, this protocol: " + url.getProtocol() + " is not supported");
+                return;
+            }
+            String host = url.getHost();
+            int port = url.getPort();
+            String file = url.getFile();
+            if (file == null) file = "/";
+            if (port <= 0) port = 80;
+            Socket socket = null;
+            try {
+                socket = new Socket(host, port);
+                String request = "GET " + file + "HTTP/1.1\r\n" +
+                                 "User-Agent: SocketTyper\r\n" +
+                                 "Accept: Text/*\r\n" +
+                                 "Host: " + host + "\r\n" +
+                                 "\r\n";
+                        /*"GET " + file + " HTTP/1.1\r\n"
+                                + "User-Agent: SocketTyper\r\n"
+                                + "Accept: Text/*\r\n" + "Host: " + host + "\r\n"
+                        + "\r\n";*/
+                byte[] b = request.getBytes("US-ASCII");
+                OutputStream output = socket.getOutputStream();
+                InputStream input = socket.getInputStream();
+                output.write(b);
+                output.flush();
+                for (int c = input.read(); c != -1; c = input.read()) {
+                    System.out.write(c);
+                }
+            } catch (IOException ex) {
+                System.err.println(ex);
+            } finally {
+                if (socket != null && socket.isConnected()) {
+                    try {
+                        socket.close();
+                    } catch (IOException ex){
+                        System.err.println(ex);
+                    }
+                }
+            }
+        } catch (MalformedURLException ex) {
+            System.err.println(ex);
+        }
     }
 }
